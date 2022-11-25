32a051f07d6565bd1db73907f5d2bdb4ba973839
Shams Sujon <sujon2762@gmail.com>
2022-11-26 02:46:57 +0600

    Basic Layout

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../sections/Navbar";
+import Footer from "../sections/Footer";
 
 const MainLayout = () => {
-    return (
-        <div>
-            <Outlet></Outlet>
-        </div>
-    );
+	return (
+		<div className="flex min-h-screen flex-col justify-between">
+			<Navbar></Navbar>
+			<div className="flex-1">
+				<Outlet></Outlet>
+			</div>
+			<Footer></Footer>
+		</div>
+	);
 };
 
 export default MainLayout;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			screens: {
+				sm: "576px",
+				md: "768px",
+				lg: "960px",
+				xl: "1280px",
+				"2xl": "1440px",
+			},
+		},
+		container: {
+			center: true,
+			padding: "1.5rem",
+		},
 	},
 	plugins: [],
 });
