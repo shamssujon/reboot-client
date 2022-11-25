49cb9f22092018e98e8f41fc8d7cb42772e28ca5
Shams Sujon <sujon2762@gmail.com>
2022-11-26 01:48:54 +0600

    Install Material Tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import { Button } from "@material-tailwind/react";
 const App = () => {
 	return (
 		<div className="App">
 			<h1 className="text-6xl">Hello</h1>
+			<Button>Button</Button>
 		</div>
 	);
 };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
+import { ThemeProvider } from "@material-tailwind/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
 	<React.StrictMode>
-		<App />
+		<ThemeProvider>
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>
 );
 
