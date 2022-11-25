226c1f0466520a8bd106b8e253a74ffc8d64d984
Shams Sujon <sujon2762@gmail.com>
2022-11-26 04:53:45 +0600

    Change project name

--- src/firebase/Firebase.config.js
+++ src/firebase/Firebase.config.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAnalytics } from "firebase/analytics";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
 	apiKey: process.env.REACT_APP_apiKey,
 	authDomain: process.env.REACT_APP_authDomain,
@@ -11,9 +8,11 @@ const firebaseConfig = {
 	storageBucket: process.env.REACT_APP_storageBucket,
 	messagingSenderId: process.env.REACT_APP_messagingSenderId,
 	appId: process.env.REACT_APP_appId,
+	measurementId: process.env.REACT_APP_measurementId,
 };
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
 
 export default firebaseApp;
