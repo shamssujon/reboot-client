0e73d6299fc0c84246ca7b19fac1f78a2103e3dd
Shams Sujon <sujon2762@gmail.com>
2022-11-26 17:03:21 +0600

    Hero update

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -1,22 +1,34 @@
 import React from "react";
 import bannerImg from "../assets/images/pc-accessories.png";
 import bannerBg from "../assets/images/banner-bg.png";
+import { Link } from "react-router-dom";
+import { Button } from "@material-tailwind/react";
 
 const Hero = () => {
 	return (
-		<section className="relative py-10 text-white lg:py-20">
-			<img src={bannerBg} alt="" className="absolute inset-0 h-full w-full object-cover -z-10" />
+		<section className="relative py-10 text-white">
+			<img src={bannerBg} alt="" className="absolute inset-0 -z-10 h-full w-full object-cover md:hidden" />
 			<div className="container">
-				<div className="grid items-center gap-10 lg:grid-cols-2">
+				<div className="relative grid items-center gap-10 overflow-hidden md:rounded-3xl md:p-20 lg:grid-cols-2">
+					<img
+						src={bannerBg}
+						alt=""
+						className="absolute inset-0 -z-10 hidden h-full w-full object-cover md:block"
+					/>
 					<div className="">
-						<h2 className="mb-4 text-4xl font-bold lg:text-5xl lg:leading-[1.1]">
+						<h2 className="mb-4 text-4xl font-bold md:text-5xl lg:leading-[1.1]">
 							Largest pre-owned PC accessories market
 						</h2>
-						<p className="text-lg">
+						<p className="mb-8 text-lg">
 							Reboot is the most popular and largest marketplace for buying and selling pre-owned
 							computer, parts and accessories. Discover what you need and sell all sorts of computer
 							products in our simple yet powerful platform. Start buying and selling today!{" "}
 						</p>
+						<Link to="/products">
+							<Button variant="filled" size="lg" className="text-lg font-normal tracking-wide">
+								Browse Products
+							</Button>
+						</Link>
 					</div>
 					<div>
 						<img src={bannerImg} alt="" className="mx-auto block h-auto max-w-full" />
