c415bcc76bd216e4f2435bd1961f7b29d23647c8
Shams Sujon <sujon2762@gmail.com>
2022-11-26 06:16:15 +0600

    Header design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
-const withMT = require("@material-tailwind/react/utils/withMT");
-
 /** @type {import('tailwindcss').Config} */
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = withMT({
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
@@ -11,6 +13,9 @@ module.exports = withMT({
 				lg: "960px",
 				xl: "1280px",
 				"2xl": "1440px",
+			},
+			fontFamily: {
+				sans: ["Kanit", ...defaultTheme.fontFamily.sans],
 			},
 		},
 		container: {
