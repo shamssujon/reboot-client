0eb410023471c41f14d9b941e7df4d1e9060e66f
Shams Sujon <sujon2762@gmail.com>
2022-11-30 17:32:20 +0600

    Fix header error

--- src/sections/Header.js
+++ src/sections/Header.js
@@ -7,15 +7,15 @@ import {
 	MenuList,
 	MobileNav,
 	Navbar,
-	Typography
+	Typography,
 } from "@material-tailwind/react";
 import React, { useContext, useEffect, useState } from "react";
-import { BsFillPersonFill } from "react-icons/bs";
+import { BsFillPersonFill, BsPerson } from "react-icons/bs";
 import { Link, NavLink } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthProvider";
 
 const Header = () => {
-	const { user, logout, } = useContext(AuthContext);
+	const { user, logout } = useContext(AuthContext);
 	const [openNav, setOpenNav] = useState(false);
 
 	useEffect(() => {
@@ -74,7 +74,8 @@ const Header = () => {
 											className="h-full w-full object-cover text-lg font-bold uppercase text-white"
 										/>
 									) : (
-										<p className="text-lg font-bold uppercase text-white">{user?.displayName[0]}</p>
+										<BsPerson className="text-xl text-white" />
+										// <p className="text-lg font-bold uppercase text-white">{user?.displayName[0]}</p>
 									)}
 								</button>
 							</MenuHandler>
